4a511d08d5a31f8a4c42022a5943e72deb997a59
krishnakurade010-cmyk <krishnakurade010@gmail.com>
2026-08-28 21:34:58 +0530

    Add factorial Python program

---
+++ factorial.py
@@ -0,0 +1,11 @@
+# Calculate the factorial of a number
+
+number = int(input("Enter a number: "))
+factorial = 1
+
+if number < 0:
+    print("Factorial is not defined for negative numbers.")
+else:
+    for i in range(1, number + 1):
+        factorial *= i
+    print("Factorial:", factorial)
